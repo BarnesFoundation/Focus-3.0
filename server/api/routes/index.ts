dd67c2fb34c12c8de072a64a186b10b3b374ac64
Christopher Jativa <cjativa@barnesfoundation.org>
2022-05-12 00:39:31 -0400

    working on route index

--- server/api/routes/index.ts
+++ server/api/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import ScanRouter from "./scanRouter";
 import ArtworkRouter from "./artworkRouter";
+import JobRouter from "./jobRouter";
 
 const ApiRouter = Router();
 
 ApiRouter.use("/scan", ScanRouter);
 ApiRouter.use("/artwork", ArtworkRouter);
+ApiRouter.use("/job", JobRouter);
 
 export default ApiRouter;
