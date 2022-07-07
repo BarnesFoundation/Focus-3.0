6f88018d434dbadd17a9d37034864adaa79efd2e
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-06 20:46:36 -0400

    add type for the session data object

--- server/api/types/sessionType.ts
+++ server/api/types/sessionType.ts
@@ -1,9 +1,12 @@
 import "express-session";
 
+export interface SessionObjectInterface {
+  initialized: boolean;
+  user_scanned_history: any[];
+  lang_pref: null | string;
+  blob: {};
+}
+
 declare module "express-session" {
-  interface SessionData {
-    initialized: boolean;
-    user_scanned_history: any[];
-    lang_pref: null | string;
-  }
+  interface SessionData extends SessionObjectInterface {}
 }
