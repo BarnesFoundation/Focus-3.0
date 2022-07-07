5b2e006dcc53982190fbe9422aafeb7d09f22a4c
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-06 22:06:15 -0400

    new middleware for capturing scanned history, adding to index file

---
+++ server/api/middleware/captureScannedHistoryMiddleware.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const captureScannedHistoryMiddleware = async (
+  request: express.Request,
+  _: express.Response,
+  next: express.NextFunction
+) => {
+  if (request.session.initialized) {
+    const artworkId = request.params.artworkId;
+    const sessionId = parseInt(request.sessionID);
+
+    // Add the artwork id to the existing scanned history
+    request.session.user_scanned_history.push(artworkId);
+
+    // Check if this session has created a bookmark before
+    // and if they have, attempt to get the email from there
+    const bookmarkThatHasEmail = await prisma.bookmarks.findFirst({
+      where: {
+        session_id: sessionId,
+        NOT: [{ email: undefined }],
+      },
+    });
+
+    // Generate a new bookmark record for this artwork
+    const now = new Date(Date.now()).toISOString();
+    await prisma.bookmarks.create({
+      data: {
+        session_id: sessionId,
+        image_id: artworkId,
+        created_at: now,
+        updated_at: now,
+        email: bookmarkThatHasEmail ? bookmarkThatHasEmail.email : null,
+        language: request.session.lang_pref,
+      },
+    });
+  }
+  next();
+};
