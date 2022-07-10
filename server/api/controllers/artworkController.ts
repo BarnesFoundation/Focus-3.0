f265e8e7196ea25477075a704dfe312d35159bcd
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-09 20:14:08 -0400

    logic for marking a story as read

--- server/api/controllers/artworkController.ts
+++ server/api/controllers/artworkController.ts
@@ -79,6 +79,43 @@ class ArtworkController {
   ) {
     const artworkId = request.params.artworkId;
     const storyId = request.params.storyId;
+    const sessionId = request.sessionID as any;
+
+    // Look up the bookmark for this artwork for the user
+    const bookmarks = await prisma.bookmarks.findMany({
+      where: {
+        image_id: artworkId,
+        session_id: sessionId,
+      },
+    });
+
+    if (bookmarks.length > 0 && request.session.blob.hasOwnProperty(storyId)) {
+      request.session.blob[storyId].read = true;
+      await prisma.bookmarks.updateMany({
+        where: {
+          image_id: artworkId,
+          session_id: sessionId,
+        },
+        data: { story_read: true },
+      });
+
+      return response.status(200).json({
+        data: {
+          success: true,
+        },
+        message: "Story has been marked as read successfully!",
+      });
+    }
+
+    // Otherwise, somehow there's no bookmark entry for any artworks related to this story
+    else {
+      return response.status(404).json({
+        data: {
+          success: false,
+        },
+        message: "Entry not found!",
+      });
+    }
   }
 }
 
