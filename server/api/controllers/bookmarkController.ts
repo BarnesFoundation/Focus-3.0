34ae975362da57e92c8452461d61fd305a4e016e
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-09 22:13:09 -0400

    subscribe to newsletter when necessary, update bookmarks language when language is set

--- server/api/controllers/bookmarkController.ts
+++ server/api/controllers/bookmarkController.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+import { BookmarkService } from "../services";
+
 const prisma = new PrismaClient();
 
 class BookmarkController {
-  public static setLanguagePreference(
+  public static async setLanguagePreference(
     request: express.Request,
     response: express.Response
   ) {
     const languageToSet = request.body.language;
+    const sessionId = request.sessionID as any;
 
     if (!languageToSet) {
       return response.status(422).json({
@@ -17,11 +20,20 @@ class BookmarkController {
       });
     }
 
-    // We need to update all bookmark entries for the user to use the new language
-    // Bookmark.where( session_id: session.id ).update_all( language: language )
-
     // Update the language for the user
     request.session.lang_pref = languageToSet.toLowerCase();
+
+    // We need to update all bookmark entries for the user to use the new language
+    // TODO - this call will fail since the `sessionId` is a string
+    // but Prisma expects `session_id` to be an auto-incremented integer
+    await prisma.bookmarks.updateMany({
+      where: {
+        session_id: sessionId,
+      },
+      data: {
+        language: languageToSet,
+      },
+    });
 
     return response
       .status(200)
@@ -33,6 +45,7 @@ class BookmarkController {
     response: express.Response
   ) {
     const email = request.body.email;
+    const newsletter = request.body;
     const sessionId = request.sessionID as any;
 
     // When we receive the email from the user
@@ -41,6 +54,7 @@ class BookmarkController {
     // TODO - this call will fail since the `sessionId` is a string
     // but Prisma expects `session_id` to be an auto-incremented integer
     if (email) {
+      let responseMessage = "Saved the email provided";
       await prisma.bookmarks.updateMany({
         where: {
           session_id: sessionId,
@@ -50,9 +64,14 @@ class BookmarkController {
         },
       });
 
+      // Subscribe the user to the newsletter if they opted-in
+      if (newsletter) {
+        await BookmarkService.subscribeUserNewsletter(email);
+        responseMessage += " and subscribed email to newsletter";
+      }
+
       return response.status(200).json({
-        data: { errors: [] },
-        message: "Saved the email provided",
+        data: { errors: [], message: responseMessage },
       });
     }
 
