30b3c7c7db940d64f80c31f958bde2eebe4edb0e
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-09 22:12:22 -0400

    create BookmarkService for subscribing to newsletter

--- server/api/services/index.ts
+++ server/api/services/index.ts
@@ -2,10 +2,12 @@ import ElasticSearchService from "./elasticSearchService";
 import ArtworkService from "./artworkService";
 import GraphCMSService from "./graphCMSService";
 import TranslateService from "./translateService";
+import BookmarkService from "./bookmarkService";
 
 export {
   ElasticSearchService,
   ArtworkService,
   GraphCMSService,
   TranslateService,
+  BookmarkService,
 };
