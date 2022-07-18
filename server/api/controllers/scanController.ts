b57b86518f5eb26c0c86c688d61d0311a7978f96
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-18 19:33:41 -0400

    working on storing the search image attempt

--- server/api/controllers/scanController.ts
+++ server/api/controllers/scanController.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import multer from "multer";
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import { promisify } from "util";
+
+const prisma = new PrismaClient();
 
 export const uploadMiddleware = multer({ dest: "uploads/" });
 export const fieldName = "image";
@@ -33,19 +38,56 @@ interface TypedRequest extends express.Request {
   body: FoundImageScan | NoFoundImageScan;
 }
 
+const unlinkAsync = promisify(fs.unlink);
+
 class ScanController {
   public static async saveScan(
     request: TypedRequest,
     response: express.Response
   ) {
+    const now = new Date(Date.now()).toISOString();
+    const sessionId = request.sessionID as any;
     const queryImage = request.file;
+    const referenceImageUrl =
+      request.body.searchSuccess === TRUE
+        ? request.body.referenceImageUrl
+        : null;
 
-    if (request.body.searchSuccess === TRUE) {
-      const requestBody = request.body;
-    } else {
-      const requestBody = request.body;
-      return response.status(200).json("image");
+    // TODO - session_d needs to be a unique field in the database but currently is not
+    const sessionAlbum = await prisma.albums.upsert({
+      where: {
+        session_id: sessionId,
+        unique_identifier: request.body.scanSeqId,
+      },
+      update: {},
+      create: {
+        session_id: sessionId,
+        unique_identifier: request.body.scanSeqId,
+      },
+    });
+
+    // Once we've created the album for the session
+    // we can create photos related to this album
+    if (sessionAlbum) {
+      const createdAlbumPhoto = await prisma.photos.create({
+        data: {
+          searched_image_blob: queryImage.buffer.toString("base64"),
+          es_response: request.body.esResponse,
+          response_time: request.body.searchTime,
+          result_image_url: referenceImageUrl,
+          search_engine: "Catchoom API",
+
+          updated_at: now,
+          created_at: now,
+        },
+      });
+
+      // We'll queue the actual upload for this image to S3
+
+      // Delete the Multer file from our local storage
+      await unlinkAsync(queryImage.path);
     }
+    response.status(200).json("Image was stored");
   }
 }
 
