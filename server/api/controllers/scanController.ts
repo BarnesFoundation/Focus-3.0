f6cec39faa0e10e14fb7c784ee7d3e49e97dc5a6
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-19 22:03:42 -0400

    create album if non existent

--- server/api/controllers/scanController.ts
+++ server/api/controllers/scanController.ts
@@ -55,44 +55,51 @@ class ScanController {
         ? request.body.referenceImageUrl
         : null;
 
-    // TODO - session_d needs to be a unique field in the database but currently is not
-    const sessionAlbum = await prisma.albums.upsert({
+    // Look up the album for this session and scan sequence
+    let sessionAlbum = await prisma.albums.findFirst({
       where: {
-        session_id: sessionId,
         unique_identifier: request.body.scanSeqId,
-      },
-      update: {},
-      create: {
         session_id: sessionId,
-        unique_identifier: request.body.scanSeqId,
       },
     });
 
-    // Once we've created the album for the session
-    // we can create photos related to this album
-    if (sessionAlbum) {
-      const createdAlbumPhoto = await prisma.photos.create({
+    // If none exists, we'll create it
+    if (!sessionAlbum) {
+      sessionAlbum = await prisma.albums.create({
         data: {
-          searched_image_blob: queryImage.buffer.toString("base64"),
-          es_response: request.body.esResponse,
-          response_time: request.body.searchTime,
-          result_image_url: referenceImageUrl,
-          search_engine: "Catchoom API",
+          session_id: sessionId,
+          unique_identifier: request.body.scanSeqId,
 
           updated_at: now,
           created_at: now,
         },
       });
-
-      // We'll queue the actual upload for this image to S3
-      await ImageUploadJob.performLater(
-        parseInt(sessionAlbum.id.toString()),
-        parseInt(createdAlbumPhoto.id.toString())
-      );
-
-      // Delete the Multer file from our local storage
-      await unlinkAsync(queryImage.path);
     }
+
+    // Once we've created the album for the session
+    // we can create photos related to this album
+    const createdAlbumPhoto = await prisma.photos.create({
+      data: {
+        searched_image_blob: queryImage.buffer.toString("base64"),
+        es_response: request.body.esResponse,
+        response_time: request.body.searchTime,
+        result_image_url: referenceImageUrl,
+        search_engine: "Catchoom API",
+
+        updated_at: now,
+        created_at: now,
+      },
+    });
+
+    // We'll queue the actual upload for this image to S3
+    await ImageUploadJob.performLater(
+      parseInt(sessionAlbum.id.toString()),
+      parseInt(createdAlbumPhoto.id.toString())
+    );
+
+    // Delete the Multer file from our local storage
+    await unlinkAsync(queryImage.path);
+
     response.status(200).json("Image was stored");
   }
 }
