ed440ba380a96e948380e1051483fe2a90e47689
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-23 12:40:18 -0400

    implement SessionClearJob code and controller for route

--- server/api/controllers/jobController.ts
+++ server/api/controllers/jobController.ts
@@ -1,6 +1,11 @@
 import express from "express";
 
-import { ElasticSearchSyncJob, StorySyncJob, ImageUploadJob } from "../jobs";
+import {
+  ElasticSearchSyncJob,
+  StorySyncJob,
+  ImageUploadJob,
+  SessionClearJob,
+} from "../jobs";
 
 class JobController {
   public static async performElasticSearchSync(
@@ -10,7 +15,7 @@ class JobController {
     await ElasticSearchSyncJob.main();
     return response
       .status(200)
-      .json("Elastic Search Sync Job completed successfully");
+      .json("ElasticSearchSyncJob completed successfully");
   }
 
   public static async performStorySyncJob(
@@ -18,7 +23,7 @@ class JobController {
     response: express.Response
   ) {
     await StorySyncJob.main();
-    return response.status(200).json("Story Sync Job completed successfully");
+    return response.status(200).json("StorySyncJob completed successfully");
   }
 
   public static async performImageUploadJob(
@@ -28,6 +33,18 @@ class JobController {
     const [albumId, photoId] = request.body;
     await ImageUploadJob.main(albumId, photoId);
     return response.status(200).json("ImageUploadJob completed successfully");
+  }
+
+  public static async performSessionClearJob(
+    request: express.Request,
+    response: express.Response
+  ) {
+    const clearedSessionsCount = await SessionClearJob.main();
+    return response
+      .status(200)
+      .json(
+        `SessionClearJob completed successfully. ${clearedSessionsCount} expired sessions cleared`
+      );
   }
 }
 
---
+++ server/api/jobs/sessionClearJob.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+import { Store } from "../../utils";
+
+const prisma = new PrismaClient();
+
+const prune = () =>
+  new Promise((resolve, reject) => {
+    Store.pruneSessions((err) => {
+      if (err) {
+        console.log(
+          `An error occurred pruning expired sessions from database`,
+          err
+        );
+        reject(err);
+      }
+      resolve("");
+    });
+  });
+
+class SessionClearJob {
+  public static async main() {
+    const expiredSessions = await prisma.user_sessions.findMany({
+      where: {
+        // Get sessions where the expiration is less than the current time
+        // which means the session is expired and can be cleared
+        expire: {
+          lte: new Date(),
+        },
+      },
+    });
+
+    console.log(
+      `SessionClearJob identified ${expiredSessions.length} expired sessions. These will be cleared from the session store.`
+    );
+    await prune();
+
+    return expiredSessions.length;
+  }
+}
+
+export default SessionClearJob;
