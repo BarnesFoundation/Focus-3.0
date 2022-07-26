351847605ec880b711ee647306aaf7e4c4fb5426
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-25 21:38:06 -0400

    add controller and route for the BookmarkDeliveryJob

--- server/api/routes/jobRouter.ts
+++ server/api/routes/jobRouter.ts
@@ -7,5 +7,9 @@ const JobRouter = Router();
 JobRouter.post("/ElasticSearchSyncJob", JobController.performElasticSearchSync);
 JobRouter.post("/ImageUploadJob", JobController.performImageUploadJob);
 JobRouter.post("/SessionClearJob", JobController.performSessionClearJob);
+JobRouter.post(
+  "/BookmarkDeliveryJob",
+  JobController.performBookmarkDeliveryJob
+);
 
 export default JobRouter;
