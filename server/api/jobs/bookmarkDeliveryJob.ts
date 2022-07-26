63c6e6e49c28e9f3a3fae1a89fc8ad41f165f451
Christopher Jativa <cjativa@barnesfoundation.org>
2022-07-25 21:13:02 -0400

    implemented 95% of the logic for bookmark delivry, still need the actual HTML template processor and mailer

--- server/api/jobs/bookmarkDeliveryJob.ts
+++ server/api/jobs/bookmarkDeliveryJob.ts
@@ -1,17 +1,23 @@
-import { PrismaClient } from "@prisma/client";
+import {
+  Prisma,
+  PrismaClient,
+  bookmarks,
+  es_cached_records,
+} from "@prisma/client";
 
 import { ElasticSearchService } from "../services";
+import { groupBy } from "../utils";
 
 const prisma = new PrismaClient();
 
-function groupBy<T>(list: Array<T>, identifier: string): Array<T> {
-  const groupedResultList = list.reduce(function (acc, item) {
-    acc[item[identifier]] = acc[item[identifier]] || [];
-    acc[item[identifier]].push(item);
-    return acc;
-  }, Object.create(null));
-  return groupedResultList;
-}
+// When this bookmark delivery job runs, we will collect bookmarks for each
+// email that occurred more than 3 hours ago
+const LATEST_BOOKMARK_ENTRY_THRESHOLD_HOURS = 3;
+const LATEST_BOOKMARK_ENTRY_THRESHOLD_SECONDS =
+  LATEST_BOOKMARK_ENTRY_THRESHOLD_HOURS * 3600;
+
+type DeliverableBookmarks = { [email: string]: bookmarks[] };
+type CollectedArtworks = { [artworkId: string]: es_cached_records };
 
 class BookmarkDeliveryJob {
   /** Job responsible for sending the email to Focus users containing all of the bookmarks
@@ -19,10 +25,70 @@ class BookmarkDeliveryJob {
    */
 
   public static async main() {
-    // Bookmarks that are ready for delivery must meet this criteria
-    // 1. Email must be defined and non-empty
-    // 2. The email for that bookmark must not have been sent already
-    const deliverableBookmarks = await prisma.bookmarks.findMany({
+    const deliverableBookmarks =
+      await BookmarkDeliveryJob.getDeliverableBookmarks();
+    console.log(
+      `Identified ${
+        Object.keys(deliverableBookmarks).length
+      } email addresses that have bookmarks ready for delivery`
+    );
+
+    // We'll pre-fetch all of the artworks associated with these bookmarks
+    // in one big batch, rather than pulling them individually from the database later
+    const artworks = await BookmarkDeliveryJob.retrieveArtworksForBookmarks(
+      deliverableBookmarks
+    );
+
+    for (const email in deliverableBookmarks) {
+      const bookmarkSet = deliverableBookmarks[email];
+      const preferredLanguage = bookmarkSet[0].language?.toLowerCase() || "en";
+
+      // Get the artworks needed for this email's set of bookmarks
+      const bookmarkArtworkList = Object.values(
+        bookmarkSet.reduce<CollectedArtworks>((acc, bookmark) => {
+          acc[bookmark.image_id] = artworks[bookmark.image_id];
+          return acc;
+        }, {})
+      );
+
+      // TODO - implement the BookmarkNotifierMailer.send_activity_email method
+      // from the Focus Ruby on Rails implementation. That logic involves
+      // bringing in templating logic and the HTML for the email that's sent
+      console.log(
+        `For email ${email}, delivered the following artwork id's`,
+        bookmarkArtworkList.map((item) => item.image_id)
+      );
+      /* BookmarkNotifierMailer.send_activity_email(mail, els_arr, language)
+        .deliver_now; */
+
+      // We'll update these bookmarks in the database to indicate the email for
+      // these bookmarks have now been processed and sent out to the user
+      const mailedBookmarkIds = bookmarkSet.map((bookmark) => bookmark.id);
+      await prisma.bookmarks.updateMany({
+        where: {
+          id: {
+            in: mailedBookmarkIds,
+          },
+          email,
+        },
+        data: {
+          mail_sent: true,
+        },
+      });
+    }
+  }
+
+  // Bookmarks that are ready for delivery must meet this criteria
+  // 1. Email must be defined and non-empty
+  // 2. The email for that bookmark must not have been sent already
+  // 3. The latest bookmark for the user must have occurred more than 3
+  // hours ago (which would indicate the user has likely finished their Focus session)
+  private static async getDeliverableBookmarks(): Promise<DeliverableBookmarks> {
+    const identifiedDeliverableBookmarks = {};
+    const threeHoursAgoTime =
+      Date.now() - LATEST_BOOKMARK_ENTRY_THRESHOLD_SECONDS;
+
+    const recentBookmarks = await prisma.bookmarks.findMany({
       where: {
         NOT: [{ email: undefined }, { email: "" }],
         mail_sent: false,
@@ -31,13 +97,57 @@ class BookmarkDeliveryJob {
         created_at: "desc",
       },
     });
-    console.log(
-      `Identified ${deliverableBookmarks.length} bookmarks ready for delivery`
-    );
 
-    if (deliverableBookmarks.length > 0) {
-      const emailGroupedBookmarks = groupBy(deliverableBookmarks, "email");
+    const bookmarksGroupedByEmail = groupBy(recentBookmarks, "email");
+    const distinctEmailList = Object.keys(bookmarksGroupedByEmail);
+
+    // We'll identify bookmark sets that should be mailed out
+    for (const email of distinctEmailList) {
+      const bookmarkSet = bookmarksGroupedByEmail[email];
+      const latestBookmarkEntry = bookmarkSet[0];
+
+      // If the latest bookmark was created more than 3 hours ago
+      // then it's an applicable set of bookmarks we want to deliver in this job run
+      if (latestBookmarkEntry.created_at.getTime() < threeHoursAgoTime) {
+        identifiedDeliverableBookmarks[email] = bookmarkSet;
+      }
     }
+
+    return identifiedDeliverableBookmarks;
+  }
+
+  private static async retrieveArtworksForBookmarks(
+    deliverableBookmarks: DeliverableBookmarks
+  ) {
+    const distinctArtworkIds = Object.values(deliverableBookmarks)
+      .flat()
+      .reduce((acc: string[], deliverableBookmark) => {
+        const bookmarkArtworkId = deliverableBookmark.image_id;
+
+        // Only add this artwork id if we haven't already stored it
+        if (acc.includes(bookmarkArtworkId) === false) {
+          acc.push(bookmarkArtworkId);
+        }
+
+        return acc;
+      }, []);
+
+    // Pull all of these artworks from the database
+    const artworks = await prisma.es_cached_records.findMany({
+      where: {
+        image_id: {
+          in: distinctArtworkIds,
+        },
+      },
+    });
+
+    // Convert the artworks to a hash list for easier artwork picking
+    const artworkHash = artworks.reduce<CollectedArtworks>((acc, artwork) => {
+      acc[artwork.image_id] = artwork;
+      return acc;
+    }, {});
+
+    return artworkHash;
   }
 }
 
