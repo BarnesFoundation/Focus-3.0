6382025c8e59c4611a0ac291f9cf22cd55923722
Christopher Jativa <cjativa@barnesfoundation.org>
2022-08-12 00:48:42 -0400

    bringing in email-templates package for testing

--- server/api/services/mailService.ts
+++ server/api/services/mailService.ts
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer";
+import Email from "email-templates";
 
 import { environmentConfiguration } from "../../config";
+import path from "path";
 
 const { sendGrid } = environmentConfiguration;
 
@@ -22,20 +24,43 @@ interface SendArguments {
   html: string;
 }
 
+const email = new Email({
+  message: {
+    from: "cjativa@barnesfoundation.org",
+  },
+  send: true,
+  transport: transporter,
+  preview: {
+    open: {
+      app: "chrome",
+      wait: false,
+    },
+    dir: path.join(__dirname, "..", "..", "..", "..", "emailPreviews"),
+  },
+});
+
 export default class MailService {
   /** Performs sending of the provided email contents to the desired address */
   public static async send({ subject, to, text, html }: SendArguments) {
     try {
-      const sendMailResponse = await transporter.sendMail({
+      /* const sendMailResponse = await transporter.sendMail({
         // TODO - this email address will eventually have to be pulled from the tenant configuration
         from: sendGrid.email,
         to,
         subject,
         text,
         html,
+      }); */
+      const sendEmailResponse = await email.send({
+        message: {
+          to: "cjativa@barnesfoundation.org",
+          subject: "Some subject",
+          html: "<p>Hello, a test!</p>",
+          text: "Hello, a test!",
+        },
       });
 
-      console.log("Successfully sent email ", sendMailResponse.messageId);
+      console.log("Successfully sent email ", sendEmailResponse);
     } catch (error) {
       console.log(`An error occurred sending email`, error);
     }
