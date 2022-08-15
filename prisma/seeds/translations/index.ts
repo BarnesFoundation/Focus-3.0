a8352bfa31e26cb197424894925a0f87cbd7fdec
Christopher Jativa <cjativa@barnesfoundation.org>
2022-08-15 17:04:47 -0400

    add translation seediiing

---
+++ prisma/seeds/translations/index.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const sqlPath = path.join(__dirname, "translations.sql");
+
+    const rawSql = await fs.promises.readFile(sqlPath, { encoding: "utf-8" });
+    const sqlStatements = rawSql.split(";");
+
+    for (const statement of sqlStatements) {
+      await prisma.$executeRawUnsafe(statement);
+    }
+  } catch (error) {
+    console.error(`An error occurred running translation seeding`, error);
+  }
+}
+
+main();
