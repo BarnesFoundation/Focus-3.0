38f7510bc36c1ebdf9244a59be21e85f16037fa6
Christopher Jativa <cjativa@barnesfoundation.org>
2022-08-17 00:55:44 -0400

    moving types to separate file

--- server/api/services/graphCMSService/graphCMSService.ts
+++ server/api/services/graphCMSService/graphCMSService.ts
@@ -1,46 +1,14 @@
 import axios from "axios";
-import { PrismaClient } from "@prisma/client";
 
 import Config from "../../../config";
 import {
-  GraphQLQuery,
   storiesForObjectIdQuery,
   relatedStoriesByObjectIdQuery,
 } from "./queries";
+import { GraphQLQuery, RelatedStory } from "./types";
 import { isEmpty } from "../../utils/isEmpty";
 
-const prisma = new PrismaClient();
-
 const UNIQUE_SEPARATOR = "***";
-
-type StoryParagraph = {
-  html: string;
-} | null;
-type StoryObjectId = string | null;
-
-interface RelatedStory {
-  id: string;
-  storyTitle: string;
-  alternativeHeroImageObjectID: string;
-  objectID1: StoryObjectId;
-  shortParagraph1: StoryParagraph;
-  longParagraph1: StoryParagraph;
-  objectID2: StoryObjectId;
-  shortParagraph2: StoryParagraph;
-  longParagraph2: StoryParagraph;
-  objectID3: StoryObjectId;
-  shortParagraph3: StoryParagraph;
-  longParagraph3: StoryParagraph;
-  objectID4: StoryObjectId;
-  shortParagraph4: StoryParagraph;
-  longParagraph4: StoryParagraph;
-  objectID5: StoryObjectId;
-  shortParagraph5: StoryParagraph;
-  longParagraph5: StoryParagraph;
-  objectID6: StoryObjectId;
-  shortParagraph6: StoryParagraph;
-  longParagraph6: StoryParagraph;
-}
 
 /** Class responsible for interacting with an GraphCMS project */
 export default class GraphCMSService {
