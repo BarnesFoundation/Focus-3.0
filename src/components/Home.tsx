e80f1e163c0e751b5902fdf48af9ad3623a9e9e0
Leigh Passamano <leighayna.josele@gmail.com>
2022-09-02 14:15:26 -0400

    Add OrientationContext to Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,9 @@
-import React, { Component, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { isAndroid, isIOS, isSafari, osVersion } from "react-device-detect";
-import { withRouter, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Textfit } from "react-textfit";
-import { compose } from "redux";
 
 import { UnsupportedDialogue } from "./UnsupportedDialogue";
-import withOrientation from "./withOrientation";
-import withTranslation from "./withTranslation";
 import * as constants from "./Constants";
 import home_background from "../images/barnes-v2-landing.png";
 import barnes_logo from "../images/Barnes_logo.svg";
@@ -14,26 +11,18 @@ import barnes_100_logo from "../images/Barnes_100_logo.svg";
 import close_icon from "../images/cross.svg";
 import kf_logo from "../images/knight-foundation-logo.svg";
 
-import {
-  logiPhoneModel,
-  shouldLogPermissionGrantTime,
-} from "../helpers/googleAnalyticsHelpers";
-import { resetApplication } from "../helpers/applicationHelpers";
+import { logiPhoneModel } from "../helpers/googleAnalyticsHelpers";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { TranslationContext } from "../contexts/TranslationContext";
 
 export const Home: React.FC = () => {
-  const { getLocalStorage, resetLocalStorage } = useLocalStorage();
+  const { resetLocalStorage } = useLocalStorage();
   const { getTranslation } = useContext(TranslationContext);
   const history = useHistory();
-  const [snapAttempts, setSnapAttempts] = useState(
-    getLocalStorage(constants.SNAP_ATTEMPTS) || 0
-  );
   const [userAtBarnes, setUserAtBarnes] = useState(null);
   const [unsupportedIOSVersion, setUnsupportedIOSVersion] = useState(null);
   const [unsupportedIOSBrowser, setUnsupportedIOSBrowser] = useState(null);
   const [cameraAccessible, setCameraAccessible] = useState(null);
-  const selectedLanguage = "";
   // Get year to determine whether to use centennial logo or original logo
   const today = new Date();
   const isCentennial = today.getFullYear() === 2022;
@@ -64,13 +53,13 @@ export const Home: React.FC = () => {
     // navigator.mediaDevices.getUserMedia() is only supported on iOS > 11.0 and only on Safari (not Chrome, Firefox, etc.)
     if (iOSVersion >= parseFloat("11.0")) {
       if (!isSafari) {
-        setUnsupportedIOSBrowser(true)
+        setUnsupportedIOSBrowser(true);
       }
     }
 
     // If they're not on iOS 11, it doesn't matter what browser they're using, navigator.mediaDevices.getUserMedia() will return undefined
     else {
-      setUnsupportedIOSVersion(true)
+      setUnsupportedIOSVersion(true);
     }
   };
 
@@ -81,7 +70,6 @@ export const Home: React.FC = () => {
     }
 
     try {
-
       // Attempt to access device camera
       await navigator.mediaDevices.getUserMedia({
         video: { facingMode: "environment", width: 1920, height: 1080 },
@@ -105,11 +93,7 @@ export const Home: React.FC = () => {
   };
 
   return (
-    <div
-      className="home-wrapper"
-      id="home-wrapper"
-      style={homeContainerStyle}
-    >
+    <div className="home-wrapper" id="home-wrapper" style={homeContainerStyle}>
       {/* Show the unsupported browser dialog if the browser is not supported */}
       {unsupportedIOSBrowser ? (
         <UnsupportedDialogue unsupportedIOSBrowser={true} />
@@ -160,7 +144,10 @@ export const Home: React.FC = () => {
                 </Textfit>
               </span>
             </button>
-            <button className="action-btn" onClick={() => setUserAtBarnes(false)}>
+            <button
+              className="action-btn"
+              onClick={() => setUserAtBarnes(false)}
+            >
               <span className="action-text h2">
                 <Textfit mode="single" max={25}>
                   {getTranslation("Welcome_screen", "text_4")}
@@ -187,10 +174,7 @@ export const Home: React.FC = () => {
           <div className="app-usage-alert h2">
             <div className="app-usage-msg">
               <span>{getTranslation("Visit_soon", "text_1")}</span>
-              <span>
-                {" "}
-                {getTranslation("Visit_soon", "text_2")}
-              </span>
+              <span> {getTranslation("Visit_soon", "text_2")}</span>
             </div>
             <div className="visit-online-link" style={visitOnlineLinkStyle}>
               <a
@@ -227,231 +211,3 @@ export const Home: React.FC = () => {
     </div>
   );
 };
-
-class HomeComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    // Get year to determine whether to use centennial logo or original logo
-    const today = new Date();
-    const isCentennial = today.getFullYear() === 2022;
-
-    this.state = {
-      snapAttempts: localStorage.getItem(constants.SNAP_ATTEMPTS) || 0,
-      selectedLanguage: "",
-
-      userAtBarnes: null,
-      unsupportedIOSVersion: null,
-      unsupportedIOSBrowser: null,
-      cameraAccessible: null,
-      isCentennial,
-    };
-  }
-
-  componentWillMount() {
-    resetApplication();
-  }
-
-  componentDidMount() {
-    if (isIOS) {
-      this.checkForGetUserMedia();
-    }
-  }
-
-  /** Determines if navigator.mediaDevices.getUserMedia() is available on the current iOS device */
-  checkForGetUserMedia = () => {
-    const iOSVersion = parseFloat(osVersion);
-
-    // navigator.mediaDevices.getUserMedia() is only supported on iOS > 11.0 and only on Safari (not Chrome, Firefox, etc.)
-    if (iOSVersion >= parseFloat("11.0")) {
-      if (!isSafari) {
-        this.setState({ unsupportedIOSBrowser: true });
-      }
-    }
-
-    // If they're not on iOS 11, it doesn't matter what browser they're using, navigator.mediaDevices.getUserMedia() will return undefined
-    else {
-      this.setState({ unsupportedIOSVersion: true });
-    }
-  };
-
-  onSelectYes = async () => {
-    // If this was an iOS device, log the model to Google Analytics
-    if (isIOS) {
-      logiPhoneModel();
-    }
-
-    try {
-      const startTime = Date.now();
-
-      // Attempt to access device camera
-      await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment", width: 1920, height: 1080 },
-      });
-
-      // TODO - Log the permission grant time if it took more than 900 ms
-      // shouldLogPermissionGrantTime(startTime);
-
-      // Navigate to the scan page
-      this.props.history.push({ pathname: "/scan" });
-    } catch (error) {
-      this.setState({ cameraAccessible: false });
-    }
-  };
-
-  onSelectNo = () => {
-    this.setState({ userAtBarnes: false });
-  };
-
-  clearUserAtBarnes = () => {
-    this.setState({ userAtBarnes: null });
-  };
-
-  closeCameraErrorScreen = () => {
-    // The user has seen that camera is not accessible, so essentially close the screen and reset camera access and them being at the Barnes
-    this.setState({ cameraAccessible: null, userAtBarnes: null });
-  };
-
-  render() {
-    const { unsupportedIOSBrowser, unsupportedIOSVersion } = this.state;
-
-    // Styles
-    let homeContainerStyle =
-      unsupportedIOSBrowser || unsupportedIOSVersion
-        ? { filter: "blur(10px)", transform: "scale(1.2)" }
-        : {};
-    let visitOnlineLinkStyle =
-      localStorage.getItem(constants.SNAP_LANGUAGE_PREFERENCE) === "Ja"
-        ? { fontSize: `18px` }
-        : {};
-
-    return (
-      <div
-        className="home-wrapper"
-        id="home-wrapper"
-        style={homeContainerStyle}
-      >
-        {/* Show the unsupported browser dialog if the browser is not supported */}
-        {unsupportedIOSBrowser ? (
-          <UnsupportedDialogue unsupportedIOSBrowser={true} />
-        ) : null}
-
-        {/* Show the unsupported iOS version dialog if the iOS version is not supported */}
-        {unsupportedIOSVersion ? (
-          <UnsupportedDialogue unsupportedIOSVersion={true} />
-        ) : null}
-
-        <img
-          src={home_background}
-          alt="home_background"
-          style={{ width: screen.width, height: screen.height }}
-        />
-
-        {/* Only show the initial Welcome Screen prompt if they haven't selected any value for userAtBarnes */}
-        {this.state.userAtBarnes == null && (
-          <div className="landing-screen">
-            {/* logo */}
-            {this.state.isCentennial ? (
-              <img
-                src={barnes_100_logo}
-                alt="barnes_logo"
-                className="logo-center centennial-logo"
-                role="img"
-                aria-label="Welcome to the Barnes Focus Digital Guide"
-              />
-            ) : (
-              <img
-                src={barnes_logo}
-                alt="barnes_logo"
-                className="logo-center"
-                role="img"
-                aria-label="Welcome to the Barnes Focus Digital Guide"
-              />
-            )}
-            <div className="user-loc-prompt">
-              {this.props.getTranslation("Welcome_screen", "text_1")}{" "}
-              <br aria-hidden={true} />
-              {this.props.getTranslation("Welcome_screen", "text_2")}
-            </div>
-            <div className="home-action">
-              <button className="action-btn" onClick={this.onSelectYes}>
-                <span className="action-text h2">
-                  <Textfit mode="single" max={25}>
-                    {this.props.getTranslation("Welcome_screen", "text_3")}
-                  </Textfit>
-                </span>
-              </button>
-              <button className="action-btn" onClick={this.onSelectNo}>
-                <span className="action-text h2">
-                  <Textfit mode="single" max={25}>
-                    {this.props.getTranslation("Welcome_screen", "text_4")}
-                  </Textfit>
-                </span>
-              </button>
-            </div>
-            <div className="kf-banner">
-              <img
-                src={kf_logo}
-                alt="knight_foundation_logo"
-                className="kf-logo"
-              />
-              <div className="kf-text caption">
-                {this.props.getTranslation("About", "text_2")}
-              </div>
-            </div>
-          </div>
-        )}
-
-        {/* If the user has selected that they're not at the Barnes, show this section */}
-        {this.state.userAtBarnes === false && (
-          <div>
-            <div className="app-usage-alert h2">
-              <div className="app-usage-msg">
-                <span>{this.props.getTranslation("Visit_soon", "text_1")}</span>
-                <span>
-                  {" "}
-                  {this.props.getTranslation("Visit_soon", "text_2")}
-                </span>
-              </div>
-              <div className="visit-online-link" style={visitOnlineLinkStyle}>
-                <a
-                  href="https://www.barnesfoundation.org/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {this.props.getTranslation("Visit_soon", "text_3")}
-                </a>
-              </div>
-            </div>
-            <div className="btn-close" onClick={this.clearUserAtBarnes}>
-              <img src={close_icon} alt="close" />
-            </div>
-          </div>
-        )}
-
-        {/* If the camera is not accessible */}
-        {this.state.cameraAccessible === false && (
-          <div>
-            <div className="app-usage-alert h2">
-              <div className="app-usage-msg">
-                {isIOS && <span>{constants.GET_USER_MEDIA_ERROR_IOS}</span>}
-                {isAndroid && (
-                  <span>{constants.GET_USER_MEDIA_ERROR_ANDROID}</span>
-                )}
-              </div>
-            </div>
-            <div className="btn-close" onClick={this.closeCameraErrorScreen}>
-              <img src={close_icon} alt="close" />
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default compose(
-  withOrientation,
-  withTranslation,
-  withRouter
-)(HomeComponent);
