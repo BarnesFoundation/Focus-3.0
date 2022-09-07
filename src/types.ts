6e9eabef482b9b480abe3f6993c5180c443c45a1
Leigh Passamano <leighayna.josele@gmail.com>
2022-09-07 18:05:00 -0400

    Update types

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,7 @@ export type StoryItemProps = {
   storyIndex: number;
   onStoryReadComplete: () => void;
   getSize: (scrollOffset: number, storyIndex: number) => void;
-  storyEmailPage: boolean;
+  storyEmailPage?: boolean;
   isLastStoryItem: boolean;
   progress: number;
   story: {
@@ -43,4 +43,120 @@ export type StoryItemState = {
   scrollHeight: number;
   showTitle: boolean;
   scrollOffset?: number;
+};
+
+export type ArtworkComponentProps = {
+  match: any;
+  history: any;
+  updateTranslations: () => Promise<void>;
+  getTranslation: () => void;
+};
+
+export type ArtworkComponentState = {
+  showEmailScreen: boolean;
+  emailCaptured: boolean;
+  showEmailForm: boolean;
+  emailCaptureAck: boolean;
+  imgLoaded: boolean;
+  alsoInRoomResults: any[];
+  email: string;
+  snapAttempts: number;
+  errors: { email: boolean };
+  showTitleBar: boolean;
+  storyDuration: number;
+  infoHeightUpdated: boolean;
+  infoCardDuration: number;
+  emailCardClickable: boolean;
+  storyTopsClickable: {};
+  result: ArtWorkResult;
+  selectedLanguage: LanguageOptionType;
+  stories: any[];
+  storyId: string;
+  storyTitle: string;
+  showStory: boolean;
+  artwork: ArtworkObject["artwork"];
+  roomRecords: ArtworkObject["roomRecords"];
+  storyDurationsCurrent: any[];
+  storyOffsets: any[];
+};
+
+type English = { name: "English"; code: "En"; selected: boolean };
+type Spanish = { name: "Español"; code: "Es"; selected: boolean };
+type French = { name: "Français"; code: "Fr"; selected: boolean };
+type German = { name: "Deutsch"; code: "De"; selected: boolean };
+type Italian = { name: "Italiano"; code: "It"; selected: boolean };
+type Russian = { name: "русский"; code: "Ru"; selected: boolean };
+type Chinese = { name: "中文"; code: "Zh"; selected: boolean };
+type Japanese = { name: "日本語"; code: "Ja"; selected: boolean };
+type Korean = { name: "한국어"; code: "Ko"; selected: boolean };
+
+export type LanguageOptionType =
+  | English
+  | Spanish
+  | French
+  | German
+  | Italian
+  | Russian
+  | Chinese
+  | Japanese
+  | Korean;
+
+export type ArtWorkResult = {
+  data: {
+    records: {
+      id: number;
+      room: string;
+      invno: string;
+      title: string;
+      medium: string;
+      people: string;
+      culture: string;
+      birthDate: string;
+      deathDate: string;
+      locations: string;
+      creditLine: string;
+      dimensions: string;
+      displayDate: string;
+      imageSecret: string;
+      nationality: string;
+      ensembleIndex: string;
+      classification: string;
+      shortDescription: string;
+      visualDescription: string;
+      curatorialApproval: string;
+      art_url: string;
+    }[];
+    roomRecords;
+    message: string;
+    show_story: boolean;
+    specialExhibition: boolean;
+  };
+  success: boolean;
+  requestComplete: boolean;
+};
+
+export type ArtworkObject = {
+  artwork: {
+    id?: number;
+    title?: string;
+    shortDescription?: string;
+    artist?: string;
+    nationality?: string;
+    birthDate?: string;
+    deathDate?: string;
+    culture?: string;
+    classification?: string;
+    locations?: string;
+    medium?: string;
+    invno?: string;
+    displayDate?: string;
+    dimensions?: string;
+    visualDescription?: string;
+    url?: string;
+    url_low_quality?: string;
+    bg_url?: string;
+    curatorialApproval?: boolean;
+    unIdentified?: boolean;
+  };
+  roomRecords: any[];
 };
