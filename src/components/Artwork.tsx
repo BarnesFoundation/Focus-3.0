0b3a49db88b09ca0ee39f81ea7a9bb6cfdf97fa2
Leigh Passamano <leighayna.josele@gmail.com>
2022-09-07 10:07:48 -0400

    Move artwork card render logic into its own component

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { compose } from "redux";
-import $ from 'jquery';
-import classnames from "classnames";
+import $ from "jquery";
 
 import * as constants from "../constants";
 import withOrientation from "./withOrientation";
@@ -11,9 +10,7 @@ import withTranslation from "./withTranslation";
 import { LanguageDropdown } from "./LanguageDropdown";
 import { EmailForm } from "./EmailForm";
 
-import google_logo from "../images/google_translate.svg";
 import { SearchRequestService } from "../services/SearchRequestService";
-import ProgressiveImage from "react-progressive-image";
 import { Controller, Scene } from "react-scrollmagic";
 // @ts-ignore
 import styled, { css } from "styled-components";
@@ -24,7 +21,7 @@ import ScrollMagic from "scrollmagic";
 import { isAndroid, isIOS } from "react-device-detect";
 
 import { ScanButton } from "./ScanButton";
-import { Share } from "./Share";
+import { ResultCard } from "./ResultCard";
 
 /**
  * withRouter HOC provides props with location, history and match objects
@@ -565,200 +562,6 @@ class Artwork extends Component {
     this.setState({ imgLoaded: true });
   };
 
-  /* Renders the focused artwork card */
-  renderArtwork = () => {
-    const { artwork, selectedLanguage } = this.state;
-    const shortDescFontStyle =
-      localStorage.getItem(constants.SNAP_LANGUAGE_PREFERENCE) === "Ru"
-        ? { fontSize: `14px` }
-        : {};
-
-    const { refCallbackInfo, setArtworkRef, langOptions, onSelectLanguage } =
-      this;
-
-    return (
-      <div className="container-fluid artwork-container" id="search-result">
-        <div className="row" ref={refCallbackInfo}>
-          <div className="artwork-top-bg">
-            <img
-              className="card-img-top"
-              src={artwork.bg_url}
-              alt="match_image_background"
-              aria-hidden={true}
-            />
-          </div>
-          <div className="col-12 col-md-12">
-            <div
-              id="result-card"
-              className="card"
-              data-title={artwork.title}
-              data-artist={artwork.artist}
-              data-id={artwork.id}
-              data-invno={artwork.invno}
-              data-nodesc-invno={!artwork.shortDescription ? artwork.invno : ""}
-            >
-              <div className="card-top-container">
-                <div className="card-img-overlay">
-                  <div className="card-header h1">Focused Artwork</div>
-                  <div className="card-img-result">
-                    <ProgressiveImage
-                      src={artwork.url}
-                      placeholder={artwork.url_low_quality}
-                    >
-                      {(src) => (
-                        <img
-                          src={src}
-                          alt="match_image"
-                          role="img"
-                          aria-label={`${artwork.title} by ${artwork.artist}${
-                            artwork.culture ? `, ${artwork.culture}.` : "."
-                          } ${artwork.visualDescription}`}
-                        />
-                      )}
-                    </ProgressiveImage>
-                    {/* <img src={artwork.url} alt="match_image" /> */}
-                  </div>
-                  <div className="card-artist">{artwork.artist}</div>
-                  <div className="card-title">{artwork.title}</div>
-                </div>
-              </div>
-              <div
-                className="card-body"
-                id="focussed-artwork-body"
-                ref={setArtworkRef}
-              >
-                <div className="share-wrapper">
-                  {/* Language options button */}
-                  <div className="language-dropdown-wrapper">
-                    <div className="language-dropdown">
-                      <LanguageDropdown
-                        langOptions={langOptions}
-                        selected={selectedLanguage}
-                        onSelectLanguage={onSelectLanguage}
-                      />
-                    </div>
-                  </div>
-                  
-                  {/* Share options button for Barnes Collection objects */}
-                  {/* TODO: determine if we want share button for special exhibition objects */}
-                  {!this.state.result.data.specialExhibition && (
-                    <Share
-                      shareText={this.props.getTranslation(
-                        "Result_page",
-                        "text_1"
-                      )}
-                      artwork={artwork}
-                    />
-                  )}
-                </div>
-
-                <div
-                  className="short-desc-container"
-                  ref={(elem) => (this.shortDescContainer = elem)}
-                >
-                  {artwork.shortDescription && (
-                    <div
-                      className="card-text paragraph"
-                      style={shortDescFontStyle}
-                      dangerouslySetInnerHTML={{
-                        __html: artwork.shortDescription,
-                      }}
-                    ></div>
-                  )}
-                </div>
-                {artwork.shortDescription &&
-                  selectedLanguage.code !== constants.LANGUAGE_EN && (
-                    <div className="google-translate-disclaimer">
-                      <span>Translated with </span>
-                      <img src={google_logo} alt="google_logo" />
-                    </div>
-                  )}
-                <div className="card-info">
-                  <table className="detail-table">
-                    <tbody>
-                      <tr>
-                        <td className="text-left item-label">{`${this.props.getTranslation(
-                          "Result_page",
-                          "text_3"
-                        )}:`}</td>
-                        <td className="text-left item-info">
-                          {artwork.artist}{" "}
-                          {!artwork.unIdentified && artwork.nationality
-                            ? `(${artwork.nationality}, ${artwork.birthDate} - ${artwork.deathDate})`
-                            : ""}
-                        </td>
-                      </tr>
-                      {artwork.unIdentified && (
-                        <tr>
-                          <td className="text-left item-label">
-                            {`${this.props.getTranslation(
-                              "Result_page",
-                              "text_10"
-                            )}:`}
-                          </td>
-                          <td className="text-left item-info">
-                            {artwork.culture}
-                          </td>
-                        </tr>
-                      )}
-                      <tr>
-                        <td className="text-left item-label">{`${this.props.getTranslation(
-                          "Result_page",
-                          "text_4"
-                        )}:`}</td>
-                        <td className="text-left item-info">{artwork.title}</td>
-                      </tr>
-                      <tr>
-                        <td className="text-left item-label">{`${this.props.getTranslation(
-                          "Result_page",
-                          "text_5"
-                        )}:`}</td>
-                        <td className="text-left item-info">
-                          {artwork.displayDate}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="text-left item-label">{`${this.props.getTranslation(
-                          "Result_page",
-                          "text_6"
-                        )}:`}</td>
-                        <td className="text-left item-info">
-                          {artwork.medium}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="text-left item-label">{`${this.props.getTranslation(
-                          "Result_page",
-                          "text_7"
-                        )}:`}</td>
-                        <td className="text-left item-info">
-                          {artwork.dimensions}
-                        </td>
-                      </tr>
-                      {!artwork.curatorialApproval && (
-                        <tr>
-                          <td className="text-left item-label">
-                            {`${this.props.getTranslation(
-                              "Result_page",
-                              "text_8"
-                            )}:`}
-                          </td>
-                          <td className="text-left item-info">
-                            {this.props.getTranslation("Result_page", "text_9")}
-                          </td>
-                        </tr>
-                      )}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   /** Renders the title bar with language dropdown */
   renderTitleBar = () => {
     return (
@@ -964,7 +767,16 @@ class Artwork extends Component {
 
     return (
       <SectionWipesStyled hasChildCards={hasChildCards}>
-        {this.renderArtwork()}
+        <ResultCard
+          artwork={this.state.artwork}
+          refCallbackInfo={this.refCallbackInfo}
+          setArtworkRef={this.setArtworkRef}
+          langOptions={this.langOptions}
+          selectedLanguage={this.state.selectedLanguage}
+          onSelectLanguage={this.onSelectLanguage}
+          shortDescContainer={this.shortDescContainer}
+          specialExhibition={this.state.result.data.specialExhibition}
+        />
 
         {this.renderStoryContainer()}
 
