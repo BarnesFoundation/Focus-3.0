79618747efbc9bc5f59d14aaf1a4a78f44fa40f6
Leigh Passamano <leighayna.josele@gmail.com>
2022-09-09 09:47:35 -0400

    Add in missing fn

--- server/api/controllers/artworkController.ts
+++ server/api/controllers/artworkController.ts
@@ -139,11 +139,30 @@ class ArtworkController {
     // TODO - uncomment this once translation is working
     // const session = request.session;
     // const languagePreference = session.lang_pref;
-    /* const objectData = await ArtworkService.findSpecialExhibitionObject(
+    const objectData = await ArtworkService.findSpecialExhibitionObject(
       objectId
     );
 
-    return response.status(200).json({ data: objectData }); */
+    // Manipulate the data to have save key/pairs as the ES results
+    objectData[0]["art_url"] = objectData[0].image.url;
+    objectData[0]["shortDescription"] = objectData[0].shortDescription.html;
+    objectData[0]["id"] = objectData[0].objectId;
+
+    const responseObject = {
+      data: {
+        records: objectData,
+        roomRecords: null,
+        message: "Result found",
+        // TODO: Implement stories for special exhibition objects
+        // showStory: storyInformation.hasStory,
+        showStory: false,
+        specialExhibition: true,
+      },
+      success: true,
+      requestComplete: true,
+    };
+
+    return response.status(200).json(responseObject);
   }
 }
 
