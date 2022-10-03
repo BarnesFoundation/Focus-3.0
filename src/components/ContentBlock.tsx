b6c6554c9a7aa59574a61c4aea65626f17d1e2a6
Leigh Passamano <leighayna.josele@gmail.com>
2022-10-03 09:47:05 -0400

    Refactor ContentBlock to use a switch statement to prevent checks against every block

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -17,91 +17,99 @@ type ContentBlockProps = {
 
 export const ContentBlock: React.FC<ContentBlockProps> = ({ contentBlock }) => (
   <div className="content-block">
-    {contentBlock.map((block, index) => (
-      <Fragment key={index}>
-        {/* Title Block */}
-        {block.type === ContentBlockTypes.TITLE && (
-          <div className="content-block__title-block">
-            {block.title && (
-              <div className="content-block__title-block__title">
-                {block.title}
-              </div>
-            )}
-            {block.subtitle && (
-              <div className="content-block__title-block__subtitle">
-                {block.subtitle}
-              </div>
-            )}
-          </div>
-        )}
-
-        {/* Text Block */}
-        {block.type === ContentBlockTypes.TEXT_BLOCK && (
-          <div className="content-block__text">
-            {formatHtml(block.textBlock.html)}
-          </div>
-        )}
-
-        {/* Image Block */}
-        {block.type === ContentBlockTypes.IMAGE && (
-          <figure className="content-block__image">
-            <img
-              src={block.image.url}
-              alt={block.altText ? block.altText : ""}
-            />
-            {block.caption && <figcaption>{block.caption}</figcaption>}
-          </figure>
-        )}
-
-        {/* Image Comparison Slider Block */}
-        {block.type === ContentBlockTypes.IMAGE_COMPARISON && (
-          <figure className="content-block__comparison">
-            <ReactCompareSlider
-              itemOne={
-                <ReactCompareSliderImage
-                  src={block.leftImage.image.url}
-                  alt={block.leftImage.altText ? block.leftImage.altText : ""}
-                />
-              }
-              itemTwo={
-                <ReactCompareSliderImage
-                  src={block.rightImage.image.url}
-                  alt={
-                    block.rightImage.altText ? block.rightImage.altText : ""
-                  }
-                />
-              }
-            />
-            <div className="content-block__comparison__caption">
-              {block.leftImage.caption && (
-                <figcaption className="caption-left">
-                  Left: {block.leftImage.caption}
-                </figcaption>
+    {contentBlock.map((block, index) => {
+      switch (block.type) {
+        // Title Block
+        case ContentBlockTypes.TITLE:
+          return (
+            <div className="content-block__title-block">
+              {block.title && (
+                <div className="content-block__title-block__title">
+                  {block.title}
+                </div>
               )}
-              {block.rightImage.caption && (
-                <figcaption className="caption-right">
-                  Right: {block.rightImage.caption}
-                </figcaption>
+              {block.subtitle && (
+                <div className="content-block__title-block__subtitle">
+                  {block.subtitle}
+                </div>
               )}
             </div>
-          </figure>
-        )}
+          );
 
-        {/* Video Block */}
-        {block.type === ContentBlockTypes.VIDEO && (
-          <ReactPlayer
-            className="content-block__video"
-            url={block.url}
-            volume={0}
-            muted={true}
-            width="100%"
-            height="fit-content"
-            playsinline={true}
-            pip={false}
-            controls={true}
-          />
-        )}
-      </Fragment>
-    ))}
+        // Text Block
+        case ContentBlockTypes.TEXT_BLOCK:
+          return (
+            <div className="content-block__text">
+              {formatHtml(block.textBlock.html)}
+            </div>
+          );
+
+        // Image Block
+        case ContentBlockTypes.IMAGE:
+          return (
+            <figure className="content-block__image">
+              <img
+                src={block.image.url}
+                alt={block.altText ? block.altText : ""}
+              />
+              {block.caption && <figcaption>{block.caption}</figcaption>}
+            </figure>
+          );
+
+        // Image Comparison Slider Block
+        case ContentBlockTypes.IMAGE_COMPARISON:
+          return (
+            <figure className="content-block__comparison">
+              <ReactCompareSlider
+                itemOne={
+                  <ReactCompareSliderImage
+                    src={block.leftImage.image.url}
+                    alt={block.leftImage.altText ? block.leftImage.altText : ""}
+                  />
+                }
+                itemTwo={
+                  <ReactCompareSliderImage
+                    src={block.rightImage.image.url}
+                    alt={
+                      block.rightImage.altText ? block.rightImage.altText : ""
+                    }
+                  />
+                }
+              />
+              <div className="content-block__comparison__caption">
+                {block.leftImage.caption && (
+                  <figcaption className="caption-left">
+                    Left: {block.leftImage.caption}
+                  </figcaption>
+                )}
+                {block.rightImage.caption && (
+                  <figcaption className="caption-right">
+                    Right: {block.rightImage.caption}
+                  </figcaption>
+                )}
+              </div>
+            </figure>
+          );
+
+        // Video Block
+        case ContentBlockTypes.VIDEO:
+          return (
+            <ReactPlayer
+              className="content-block__video"
+              url={block.url}
+              volume={0}
+              muted={true}
+              width="100%"
+              height="fit-content"
+              playsinline={true}
+              pip={false}
+              controls={true}
+            />
+          );
+
+        default:
+          return null;
+      }
+    })}
   </div>
 );
