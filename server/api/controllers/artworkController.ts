4b004cd06f9a74c4f629cea385a7394c89aa27ad
Leigh Passamano <leighayna.josele@gmail.com>
2022-10-14 17:48:00 -0400

    Remove unused route and controller fn

--- server/api/controllers/artworkController.ts
+++ server/api/controllers/artworkController.ts
@@ -146,41 +146,6 @@ class ArtworkController {
       });
     }
   }
-
-  public static async getSpecialExhibitionObject(
-    request: express.Request,
-    response: express.Response
-  ) {
-    const objectId = request.params.objectId;
-
-    // TODO - uncomment this once translation is working
-    // const session = request.session;
-    // const languagePreference = session.lang_pref;
-    const objectData = await ArtworkService.findSpecialExhibitionObject(
-      objectId
-    );
-
-    // Manipulate the data to have save key/pairs as the ES results
-    objectData[0]["art_url"] = objectData[0].image.url;
-    objectData[0]["shortDescription"] = objectData[0].shortDescription.html;
-    objectData[0]["id"] = objectData[0].objectId;
-
-    const responseObject = {
-      data: {
-        records: objectData,
-        roomRecords: null,
-        message: "Result found",
-        // TODO: Implement stories for special exhibition objects
-        // showStory: storyInformation.hasStory,
-        showStory: false,
-        specialExhibition: true,
-      },
-      success: true,
-      requestComplete: true,
-    };
-
-    return response.status(200).json(responseObject);
-  }
 }
 
 export default ArtworkController;
