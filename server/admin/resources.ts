0aab73977c8585977b8439ddb714e384f3868d0a
Chris Jativa <cjativa@barnesfoundation.org>
2023-09-01 00:46:41 -0400

    prevent bulkDelete of records using UI/API

--- server/admin/resources.ts
+++ server/admin/resources.ts
@@ -9,14 +9,25 @@ const dmmf = (prisma as any)._dmmf as DMMFClass;
 // @ts-ignore - This is necessary for @admin/prisma to work properly
 prisma._baseDmmf = dmmf;
 
+// Prevent editing single resources via UI and API
 const disabledEditAction = {
   edit: {
     isAccessible: false,
     isVisible: false,
   },
 };
+
+// Prevent deleting single resources via UI and API
 const disabledDeleteAction = {
   delete: {
+    isAccessible: false,
+    isVisible: false,
+  },
+};
+
+// Prevent bulk deleting resources via UI and API
+const disabledBulkDeleteAction = {
+  bulkDelete: {
     isAccessible: false,
     isVisible: false,
   },
@@ -25,11 +36,23 @@ const disabledDeleteAction = {
 export const resourceList = [
   {
     resource: { model: dmmf.modelMap.bookmarks, client: prisma },
-    options: { actions: { ...disabledEditAction, ...disabledDeleteAction } },
+    options: {
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
+    },
   },
   {
     resource: { model: dmmf.modelMap.albums, client: prisma },
-    options: { actions: { ...disabledEditAction, ...disabledDeleteAction } },
+    options: {
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
+    },
   },
   {
     resource: { model: dmmf.modelMap.photos, client: prisma },
@@ -42,19 +65,41 @@ export const resourceList = [
           },
         },
       },
-      actions: { ...disabledEditAction, ...disabledDeleteAction },
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
     },
   },
   {
     resource: { model: dmmf.modelMap.stories, client: prisma },
-    options: { actions: { ...disabledEditAction, ...disabledDeleteAction } },
+    options: {
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
+    },
   },
   {
     resource: { model: dmmf.modelMap.subscriptions, client: prisma },
-    options: { actions: { ...disabledEditAction, ...disabledDeleteAction } },
+    options: {
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
+    },
   },
   {
     resource: { model: dmmf.modelMap.translations, client: prisma },
-    options: { actions: { ...disabledEditAction, ...disabledDeleteAction } },
+    options: {
+      actions: {
+        ...disabledEditAction,
+        ...disabledDeleteAction,
+        ...disabledBulkDeleteAction,
+      },
+    },
   },
 ];
