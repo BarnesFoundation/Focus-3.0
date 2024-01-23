03b3f311d00db6ff1480b43696a819757d8d3e8f
Leigh Passamano <leighayna.josele@gmail.com>
2024-01-23 10:35:21 -0500

    Make footer full page width

--- src/components/BookmarkEmail.tsx
+++ src/components/BookmarkEmail.tsx
@@ -8,7 +8,6 @@ import instagramLogo from "../images/mailer/social_instagram.png";
 import linkedinLogo from "../images/mailer/social_linkedin.png";
 import twitterLogo from "../images/mailer/social_twitter.png";
 import youtubeLogo from "../images/mailer/social_youtube.png";
-import barnesRedLogo from "../images/mailer/barnes_email_logo_red.png";
 
 export const BookmarkEmail: React.FC = () => {
   const [bookmarks, setBookmarks] = useState<any[]>();
@@ -146,252 +145,250 @@ export const BookmarkEmail: React.FC = () => {
         </div>
       </div>
 
-      <div className="email__body">
-        {/* Email content */}
-        <div className="email__content">
-          {/* Welcome and bookmark message */}
-          <div className="email__content__message bottom-border">
-            <h2 className="email__content__message__header">
-              {translations && translations["text_1"]["translated_content"]}
-            </h2>
-            <p className="email__content__message__text">
-              {translations && translations["text_2"]["translated_content"]}
-            </p>
-            <p className="email__content__message__text">
-              Get more art in your inbox.{" "}
-              <a href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox">
-                Sign up for Barnes emails
-              </a>{" "}
-              to stay on top of special offers and happenings all year round.
-            </p>
-          </div>
+      {/* Email content */}
+      <div className="email__content">
+        {/* Welcome and bookmark message */}
+        <div className="email__content__message bottom-border">
+          <h2 className="email__content__message__header">
+            {translations && translations["text_1"]["translated_content"]}
+          </h2>
+          <p className="email__content__message__text">
+            {translations && translations["text_2"]["translated_content"]}
+          </p>
+          <p className="email__content__message__text">
+            Get more art in your inbox.{" "}
+            <a href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox">
+              Sign up for Barnes emails
+            </a>{" "}
+            to stay on top of special offers and happenings all year round.
+          </p>
+        </div>
 
-          {/* Bookmarks */}
-          <div className="email__content__bookmarks">
-            {bookmarks &&
-              bookmarks.map((bookmark, index) => (
-                <div
-                  key={index}
-                  className="email__content__bookmarks__item bottom-border"
-                >
-                  <img
-                    src={`https://barnes-images.imgix.net/${bookmark.id.toString()}_${bookmark.imageSecret.toString()}_n.jpg?crop=faces,entropy&fit=crop&w=200&h=200`}
-                    alt={bookmark.id.toString()}
-                  />
-                  <div className="email__content__bookmarks__item__text">
+        {/* Bookmarks */}
+        <div className="email__content__bookmarks">
+          {bookmarks &&
+            bookmarks.map((bookmark, index) => (
+              <div
+                key={index}
+                className="email__content__bookmarks__item bottom-border"
+              >
+                <img
+                  src={`https://barnes-images.imgix.net/${bookmark.id.toString()}_${bookmark.imageSecret.toString()}_n.jpg?crop=faces,entropy&fit=crop&w=200&h=200`}
+                  alt={bookmark.id.toString()}
+                />
+                <div className="email__content__bookmarks__item__text">
+                  <p>
+                    <a
+                      href={`https://collection.barnesfoundation.org/objects/${bookmark.id.toString()}/${bookmark.title
+                        }?utm_source=focus&utm_medium=email&utm_campaign=focus_scanned_object`}
+                      title="Barnes Collection"
+                      style={{ color: "#d6421f !important" }}
+                    >
+                      <h3>{bookmark.title}</h3>
+                    </a>
+                  </p>
+                  {bookmark["people"]}. {bookmark["title"]},
+                  {bookmark["displayDate"]}. {bookmark["medium"]},
+                  {bookmark["dimensions"]}. {bookmark["invno"]}
+                  {bookmark.objRightsTypeID && (
                     <p>
-                      <a
-                        href={`https://collection.barnesfoundation.org/objects/${bookmark.id.toString()}/${bookmark.title
-                          }?utm_source=focus&utm_medium=email&utm_campaign=focus_scanned_object`}
-                        title="Barnes Collection"
-                        style={{ color: "#d6421f !important" }}
-                      >
-                        <h3>{bookmark.title}</h3>
-                      </a>
+                      {["1", "3"].includes(bookmark["objRightsTypeID"]) ? (
+                        <Fragment>
+                          In Copyright. <br />
+                          {bookmark["creditLine"]}
+                        </Fragment>
+                      ) : ["4", "8", "10"].includes(
+                        bookmark["objRightsTypeID"]
+                      ) ? (
+                        <Fragment>Public domain.</Fragment>
+                      ) : ["2", "6"].includes(bookmark["objRightsTypeID"]) ? (
+                        <Fragment>Copyright Undetermined.</Fragment>
+                      ) : (
+                        <Fragment></Fragment>
+                      )}
                     </p>
-                    {bookmark["people"]}. {bookmark["title"]},
-                    {bookmark["displayDate"]}. {bookmark["medium"]},
-                    {bookmark["dimensions"]}. {bookmark["invno"]}
-                    {bookmark.objRightsTypeID && (
-                      <p>
-                        {["1", "3"].includes(bookmark["objRightsTypeID"]) ? (
-                          <Fragment>
-                            In Copyright. <br />
-                            {bookmark["creditLine"]}
-                          </Fragment>
-                        ) : ["4", "8", "10"].includes(
-                          bookmark["objRightsTypeID"]
-                        ) ? (
-                          <Fragment>Public domain.</Fragment>
-                        ) : ["2", "6"].includes(bookmark["objRightsTypeID"]) ? (
-                          <Fragment>Copyright Undetermined.</Fragment>
-                        ) : (
-                          <Fragment></Fragment>
-                        )}
-                      </p>
-                    )}
-                  </div>
+                  )}
                 </div>
-              ))}
+              </div>
+            ))}
+        </div>
+      </div>
+
+      {/* Email footer */}
+      <div className="email__footer">
+        {/* Footer Social Icons */}
+        <div className="email__footer__social">
+          <div className="email__footer__social__item">
+            <a
+              href="https://www.facebook.com/barnesfoundation/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={facebookLogo}
+                alt="facebook"
+                width="20px"
+                height="20px"
+                style={{ padding: "20px 10px; opacity: 0.8" }}
+              />
+            </a>
+          </div>
+          <div className="email__footer__social__item">
+            <a
+              href="https://www.instagram.com/barnesfoundation/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={instagramLogo}
+                alt="instagram"
+                width="20px"
+                height="20px"
+                style={{ padding: "20px 10px; opacity: 0.8" }}
+              />
+            </a>
+          </div>
+          <div className="email__footer__social__item">
+            <a
+              href="https://www.linkedin.com/company/barnes-foundation"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={linkedinLogo}
+                alt="linkedin"
+                width="20px"
+                height="20px"
+                style={{ padding: "20px 10px; opacity: 0.8" }}
+              />
+            </a>
+          </div>
+          <div className="email__footer__social__item">
+            <a
+              href="https://twitter.com/the_barnes"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={twitterLogo}
+                alt="twitter"
+                width="20px"
+                height="20px"
+                style={{ padding: "20px 10px; opacity: 0.8" }}
+              />
+            </a>
+          </div>
+          <div className="email__footer__social__item">
+            <a
+              href="https://www.youtube.com/user/BarnesFoundation"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={youtubeLogo}
+                alt="Youtube"
+                width="20px"
+                height="20px"
+                style={{ padding: "20px 10px; opacity: 0.8" }}
+              />
+            </a>
           </div>
         </div>
 
-        {/* Email footer */}
-        <div className="email__footer">
-          {/* Footer Social Icons */}
-          <div className="email__footer__social">
-            <div className="email__footer__social__item">
-              <a
-                href="https://www.facebook.com/barnesfoundation/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={facebookLogo}
-                  alt="facebook"
-                  width="20px"
-                  height="20px"
-                  style={{ padding: "20px 10px; opacity: 0.8" }}
-                />
-              </a>
-            </div>
-            <div className="email__footer__social__item">
-              <a
-                href="https://www.instagram.com/barnesfoundation/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={instagramLogo}
-                  alt="instagram"
-                  width="20px"
-                  height="20px"
-                  style={{ padding: "20px 10px; opacity: 0.8" }}
-                />
-              </a>
-            </div>
-            <div className="email__footer__social__item">
-              <a
-                href="https://www.linkedin.com/company/barnes-foundation"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={linkedinLogo}
-                  alt="linkedin"
-                  width="20px"
-                  height="20px"
-                  style={{ padding: "20px 10px; opacity: 0.8" }}
-                />
-              </a>
-            </div>
-            <div className="email__footer__social__item">
-              <a
-                href="https://twitter.com/the_barnes"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={twitterLogo}
-                  alt="twitter"
-                  width="20px"
-                  height="20px"
-                  style={{ padding: "20px 10px; opacity: 0.8" }}
-                />
-              </a>
-            </div>
-            <div className="email__footer__social__item">
-              <a
-                href="https://www.youtube.com/user/BarnesFoundation"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src={youtubeLogo}
-                  alt="Youtube"
-                  width="20px"
-                  height="20px"
-                  style={{ padding: "20px 10px; opacity: 0.8" }}
-                />
-              </a>
-            </div>
-          </div>
+        {/* Footer links */}
+        <div className="email__footer__links">
+          <h3 className="email__footer__links__item">
+            <a
+              href="https://www.barnesfoundation.org/whats-on/collection"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Visit
+            </a>
+          </h3>
+          <h3>
+            <a
+              href="https://www.barnesfoundation.org/whats-on/talks"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tour
+            </a>
+          </h3>
+          <h3>
+            <a
+              href="https://www.barnesfoundation.org/support"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Join
+            </a>
+          </h3>
+          <h3>
+            <a
+              href="https://shop.barnesfoundation.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Shop
+            </a>
+          </h3>
+          <h3>
+            <a
+              href="https://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Subscribe
+            </a>
+          </h3>
+        </div>
 
-          {/* Footer links */}
-          <div className="email__footer__links">
-            <h3 className="email__footer__links__item">
-              <a
-                href="https://www.barnesfoundation.org/whats-on/collection"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Visit
-              </a>
-            </h3>
-            <h3>
-              <a
-                href="https://www.barnesfoundation.org/whats-on/talks"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Tour
-              </a>
-            </h3>
-            <h3>
-              <a
-                href="https://www.barnesfoundation.org/support"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Join
-              </a>
-            </h3>
-            <h3>
-              <a
-                href="https://shop.barnesfoundation.org/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Shop
-              </a>
-            </h3>
-            <h3>
-              <a
-                href="https://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Subscribe
-              </a>
-            </h3>
+        {/* Footer address | Contact etc */}
+        <div className="email__footer__contact">
+          <div className="email__footer__contact__item">
+            2025 Benjamin Franklin Parkway, Philadelphia, PA 19130
           </div>
-
-          {/* Footer address | Contact etc */}
-          <div className="email__footer__contact">
-            <div className="email__footer__contact__item">
-              2025 Benjamin Franklin Parkway, Philadelphia, PA 19130
-            </div>
-            <div className="email__footer__contact__item">
-              <span className="no-wrap">
-                Call: <a href="tel:215-278-7000">215.278.7000</a>
-              </span>
-              {"  "}|{"  "}
-              <span className="no-wrap">
-                Email:{" "}
-                <a href="mailto:info@barnesfoundation.org">
-                  info@barnesfoundation.org
-                </a>
-              </span>
-            </div>
-            <div className="email__footer__contact__item">
-              <span className="no-wrap">
-                <a
-                  href="https://www.barnesfoundation.org/privacy-policy"
-                  style={{ textDecoration: "underline" }}
-                >
-                  Privacy Policy
-                </a>
-              </span>
-              {"  "}|{"  "}
-              <span className="no-wrap">
-                <a
-                  href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox"
-                  style={{ textDecoration: "underline" }}
-                  className="no-wrap"
-                >
-                  Email Preferences
-                </a>
-              </span>
-              {"  "}|{"  "}
-              <span className="no-wrap">
-                <a
-                  href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/opt-out"
-                  style={{ textDecoration: "underline" }}
-                >
-                  Unsubscribe
-                </a>
-              </span>
-            </div>
+          <div className="email__footer__contact__item">
+            <span className="no-wrap">
+              Call: <a href="tel:215-278-7000">215.278.7000</a>
+            </span>
+            {"  "}|{"  "}
+            <span className="no-wrap">
+              Email:{" "}
+              <a href="mailto:info@barnesfoundation.org">
+                info@barnesfoundation.org
+              </a>
+            </span>
+          </div>
+          <div className="email__footer__contact__item">
+            <span className="no-wrap">
+              <a
+                href="https://www.barnesfoundation.org/privacy-policy"
+                style={{ textDecoration: "underline" }}
+              >
+                Privacy Policy
+              </a>
+            </span>
+            {"  "}|{"  "}
+            <span className="no-wrap">
+              <a
+                href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/curate_your_inbox"
+                style={{ textDecoration: "underline" }}
+                className="no-wrap"
+              >
+                Email Preferences
+              </a>
+            </span>
+            {"  "}|{"  "}
+            <span className="no-wrap">
+              <a
+                href="http://www.pages03.net/thebarnesfoundation/EmailPreferences/opt-out"
+                style={{ textDecoration: "underline" }}
+              >
+                Unsubscribe
+              </a>
+            </span>
           </div>
         </div>
       </div>
